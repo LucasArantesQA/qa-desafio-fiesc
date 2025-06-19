2c017d5854013ddb0f0b86c202d7aacab15c8b63
Lucas <lucasarantesqa@gmail.com>
2025-06-19 17:18:37 -0300

    initial commit

---
+++ api-fiesc/src/main/java/com/lucasj/api_fiesc/ApiFiescApplication.java
@@ -0,0 +1,13 @@
+package com.lucasj.api_fiesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFiescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFiescApplication.class, args);
+	}
+
+}
---
+++ api-fiesc/src/test/java/com/lucasj/api_fiesc/ApiFiescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasj.api_fiesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFiescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
